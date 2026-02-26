6ba11682cc823cd47328494ddfaf361b8ac9d21d
lev526 <medcovlev8484@gmail.com>
2026-02-26 19:19:16 +0400

    new cycle

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -10,6 +10,10 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+            for(int i  = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
